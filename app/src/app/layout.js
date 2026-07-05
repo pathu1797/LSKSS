1612c174e6c1919a543eb09588df96bc3084495c
Prathamesh Joshi <prathameshjoshi1797@gmail.com>
2026-07-05 19:15:16 +0530

    Add Vercel Speed Insights

--- app/src/app/layout.js
+++ app/src/app/layout.js
@@ -2,7 +2,7 @@ import { Playfair_Display, Inter } from "next/font/google";
 import "./globals.css";
 import ScrollToTop from "@/components/ScrollToTop";
 import { Analytics } from "@vercel/analytics/next";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const playfair = Playfair_Display({
   variable: "--font-playfair",
@@ -36,6 +36,7 @@ export default function RootLayout({ children }) {
         {children}
         <ScrollToTop />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
